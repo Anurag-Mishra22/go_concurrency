301c654341b619e85928200aa5a43375e00d97dd
Anurag Mishra <anurag2025mishra@gmail.com>
2025-11-20 13:17:16 +0530

    feat: add async_preemptible

---
+++ gorutines_and_scheduler/async_preemptible/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"runtime"
+)
+
+func infiniteLoop(str string) {
+	for {
+		log.Println(str)
+		// runtime.Gosched()
+	}
+}
+
+func loop(str string) {
+	for i := 0; i < 5; i++ {
+		runtime.Gosched()
+		log.Println(str)
+	}
+}
+
+func main() {
+	runtime.GOMAXPROCS(1)
+	go infiniteLoop("infinite_loop")
+	loop("loop")
+	log.Println("main ends")
+}
