db5a4941e71416af859b7752c456d239d7f24aea
Anurag Mishra <anurag2025mishra@gmail.com>
2025-11-24 19:49:15 +0530

    add incorrect_defer_with_mutex example

---
+++ sync_primitives/incorrect_defer_with_mutex/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var mutex sync.Mutex
+var cache map[string]string
+
+func doSomething() {
+	var value string
+	{
+		mutex.Lock()
+		defer mutex.Unlock()
+		value = cache["key"]
+	}
+
+	fmt.Println(value)
+
+	{
+		mutex.Lock()
+		defer mutex.Unlock()
+		cache["key"] = "value"
+	}
+}
